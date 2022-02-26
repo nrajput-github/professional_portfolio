69939a0a41b8a8062a6c56d88fee5cafeb276f5b
Navya Rajput <navyasrajput@gmail.com>
2022-02-25 22:31:37 -0500

    to deploy on heroku

--- server.js
+++ server.js
@@ -11,5 +11,13 @@ app.use(express.json());
 app.use(cors());
 app.use("/", contactRoute);
 
-const port = process.env.PORT || 5001;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"))
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
+
+const port = process.env.PORT || 5000;
 app.listen(port, console.log(`server listening to port 5000`));
